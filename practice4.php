608387211d164630d0017038a9c421ff0e141afb
dzsailors <143645948+dzsailors@users.noreply.github.com>
2023-10-29 21:34:38 -0500

    upload practice 1-4

---
+++ practice4.php
@@ -0,0 +1,32 @@
+<html>
+    <style>
+        body {background-color: #1f1f1f; color: white;}
+    </style>
+    <body>
+        <h1>Multiplication Table</h1>
+    	<?php
+        if (isset($_POST["size"])) {
+            $size = intval($_POST["size"]);
+	    	if ($size > 0) {
+		    	echo "<table border='1'>";
+			    echo "<tr><th></th>";
+    			for ($i = 1; $i <= $size; $i++) {
+	    			echo "<th>" . $i . "</th>";
+		    	}
+			    echo "</tr>";
+    			for ($i = 1; $i <= $size; $i++) {
+	    			echo "<tr><th>" . $i . "</th>";
+		    		for ($j = 1; $j <= $size; $j++) {
+			    		echo "<td>" . ($i * $j) . "</td>";
+				    }
+    				echo "</tr>";
+	    		}
+		    	echo "</table>";
+    		} 
+            else {
+		    	echo "<p>Enter a valid number.</p>";
+    		}
+	    }
+    	?>
+    </body>
+</html>
